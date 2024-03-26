ae61a018008ee94a7e12e9c5ddfc3b49c99daa44
Aniket Bisht <tekinas5000@gmail.com>
2024-03-26 22:12:29 +0530

    changes

--- compile_gcc.sh
+++ compile_gcc.sh
@@ -3,9 +3,12 @@ cd gcc
 git pull
 cd ../gcc-obj/
 rm ../gcc-obj/* -rf
-#export CC="gcc-trunk -march=native"
-#export CXX="g++-trunk -march=native"
-export CC="clang -march=native"
-export CXX="clang++ -march=native"
-../gcc/configure --prefix=/usr --libdir=/usr/lib --libexecdir=/usr/lib --mandir=/usr/share/man --infodir=/usr/share/info --enable-languages=c,c++ --program-suffix=-trunk
+export CC="gcc-trunk -march=native"
+export CXX="g++-trunk -march=native"
+#export CC="clang -march=native"
+#export CXX="clang++ -march=native"
+../gcc/configure --enable-languages=c,c++ --prefix=/usr --libdir=/usr/lib --libexecdir=/usr/lib --mandir=/usr/share/man --infodir=/usr/share/info \
+    --with-linker-hash-style=gnu --with-system-zlib --enable-__cxa_atexit --enable-cet=auto --enable-clocale=gnu --enable-default-pie --enable-default-ssp \
+    --enable-gnu-indirect-function --enable-gnu-unique-object --enable-libstdcxx-backtrace --enable-link-serialization=1 --enable-linker-build-id --enable-lto --enable-multilib \
+    --enable-plugin --enable-shared --enable-threads=posix --disable-libssp --disable-libstdcxx-pch --disable-werror --program-suffix=-trunk
 time make -j (nproc)
--- compile_llvm.sh
+++ compile_llvm.sh
@@ -2,5 +2,5 @@ cd llvm-project/
 git pull
 rm ../llvm-obj/* -rf
 cd ../llvm-obj/
-cmake -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_CXX_FLAGS="-march=native" -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi" -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release -G "Ninja" ../llvm-project/llvm
+cmake -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_CXX_FLAGS="-march=native" -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld;polly;llvm" -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;compiler-rt;libc;libunwind" -DLLVM_TARGETS_TO_BUILD=host -DCMAKE_BUILD_TYPE=Release -G "Ninja" ../llvm-project/llvm
 time ninja
